955a9c5542e1bfd422063dc4f5f18a83aa05a8d3
Bianyu <by15242952083@outlook.com>
2023-07-05 15:37:25 +0800

    init easy-code-cli

---
+++ index.ts
@@ -0,0 +1,19 @@
+/*
+ * @Author: Bianyu by15242952083@outlook.com
+ * @Date: 2023-07-05 14:08:07
+ * @LastEditors: Bianyu by15242952083@outlook.com
+ * @LastEditTime: 2023-07-05 15:30:53
+ * @FilePath: \Easy-code-cli\index.ts
+ * @Description:
+ * Copyright (c) 2023 by Bianyu email: by15242952083@outlook.com, All Rights Reserved.
+ */
+import input from '@inquirer/input'
+
+import consola from 'consola'
+
+async function init() {
+  const modulesName = await input({ message: '请输入模块名称' })
+  consola.info(['模块名称', modulesName])
+}
+
+export default init
